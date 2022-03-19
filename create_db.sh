0fffe252bf138ec4c3d08b0cfbad9f278360e8a2
KevinMakau <38019900+KevinMakau@users.noreply.github.com>
2022-03-18 20:23:48 -0700

    Update create_db.sh

--- create_db.sh
+++ create_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp *.csv /tmp/$USER/myDB/data/
 psql -h localhost -p $PGPORT $USER"_DB" < create_tables.sql
 psql -h localhost -p $PGPORT $USER"_DB" < create_indexes.sql
 psql -h localhost -p $PGPORT $USER"_DB" < load_data.sql
